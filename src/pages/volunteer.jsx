ead4e52e65019d044af5805357188541aa67241a
Alonso Herrera <alonso.bkn.098@gmail.com>
2023-10-17 19:23:41 -0400

    uwu

--- src/pages/volunteer.jsx
+++ src/pages/volunteer.jsx
@@ -1,45 +1,57 @@
-import React from 'react'
-import { Button} from '@mui/material'
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-
-import NavBar from '../components/nav_bar'
-// import LogoUSM from '../assets/usm.png'
+import React, { useState } from 'react';  // Import useState from React
+import {InputLabel,Button,MenuItem,FormControl,Select, Container} from '@mui/material';
+import NavBar from '../components/nav_bar';
 
 export const Volunteer = () => {
-  const [afectados, setAfectados] = React.useState('');
-  const [catastrofe, setCatastrofe] = React.useState('');
+  const [afectados, setAfectados] = useState('');  // Use useState to initialize state
+  const [catastrofe, setCatastrofe] = useState('');  // Use useState to initialize state
 
   return (
     <div className='page'>
       <h1 className='page__title'>Chile Ayuda</h1>
       <NavBar />
       <h2>Solicitante</h2>
-      {/* <img src={LogoUSM} height={100} alt='logo usm' /><br /> */}
-      {/* <Button variant='contained'>Material UI Button</Button> */}
-      <FormControl fullWidth>
-        <InputLabel id="afectados">Personas Afectadas</InputLabel>
-          <Select labelId="afectados" id="afectados" value={afectados} label="Afectados" onChange={(e) => setAfectados(e.target.value)}>
+      <h3>Publicar busqueda de ayuda</h3>
+      <Container>
+        <FormControl fullWidth>
+          <InputLabel id="afectados-label">Personas Afectadas</InputLabel>
+          <Select
+            labelId="afectados-label"
+            id="afectados"
+            value={afectados}
+            onChange={(e) => setAfectados(e.target.value)}
+            label="Personas Afectadas"
+          >
             <MenuItem value={'1-100'}>1-100</MenuItem>
             <MenuItem value={'100-1000'}>100-1000</MenuItem>
             <MenuItem value={'1000-10000'}>1000-10000</MenuItem>
             <MenuItem value={'10000-100000'}>10000-100000</MenuItem>
             <MenuItem value={'+100000'}>+100000</MenuItem>
           </Select>
-        <InputLabel id="tipo-catastrofe">Catastrofe</InputLabel>
-          <Select labelId="tipo-catastrofe" id="tipo-catastrofe" value={catastrofe} label="catastrofe" onChange={(e) => setCatastrofe(e.target.value)}>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel id="catastrofe-label">Catastrofe</InputLabel>
+          <Select
+            labelId="catastrofe-label"
+            id="catastrofe"
+            value={catastrofe}
+            onChange={(e) => setCatastrofe(e.target.value)}
+            label="Catastrofe"
+          >
             <MenuItem value={'Derrumbe'}>Derrumbe</MenuItem>
-            <MenuItem value={'Inundacion'}>Inundacíon</MenuItem>
+            <MenuItem value={'Inundacion'}>Inundación</MenuItem>
             <MenuItem value={'Incendio'}>Incendio</MenuItem>
-            <MenuItem value={'Sequia'}>Sequia</MenuItem>
-            <MenuItem value={'Quimico'}>Quimico</MenuItem>
+            <MenuItem value={'Sequia'}>Sequía</MenuItem>
+            <MenuItem value={'Quimico'}>Químico</MenuItem>
             <MenuItem value={'Otro'}>Otro</MenuItem>
           </Select>
-      </FormControl>
+        </FormControl>
+        <Button type="submit" variant="contained" color="primary">Submit</Button>
+      </Container>
+
+
     </div>
-  )
+  );
 }
 
-export default Volunteer
+export default Volunteer;
